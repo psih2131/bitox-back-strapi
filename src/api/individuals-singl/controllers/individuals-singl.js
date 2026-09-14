54c8122e6f1311a12ae8e2bf90d7346d322a242c
psih2131 <gearsdevelope@gmail.com>
2026-09-14 14:20:23 +0300

    fix

--- src/api/individuals-singl/controllers/individuals-singl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * individuals-singl controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::individuals-singl.individuals-singl');
--- src/api/individuals-singl/services/individuals-singl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * individuals-singl service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::individuals-singl.individuals-singl');
